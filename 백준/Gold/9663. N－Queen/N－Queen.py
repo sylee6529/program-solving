08764fadceb467474438d22bafe6685468f4d6d7
Seoyeon Lee <68765200+sylee6529@users.noreply.github.com>
2023-10-16 10:10:12 +0900

    [Gold IV] Title: N-Queen, Time: 29144 ms, Memory: 198600 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,29 +1,34 @@
-def adjacent(x): # x와 i가 같으면 행이 같은거
-    for i in range(x):
-        # 열이 같거나 대각선에 위치라면 false
-        if row[x] == row[i] or abs(row[x]-row[i]) == x-i:
+import sys
+
+
+cnt = 0
+N = int(sys.stdin.readline())
+col = [-1] * N
+
+
+def n_queen(num, col):
+    global cnt
+
+    if num == N:
+        cnt += 1
+        return
+
+    for i in range(N):
+        col[num] = i
+
+        if promising(num, col):
+            n_queen(num + 1, col)
+
+
+def promising(r, col):
+    k = 0
+    flag = True
+    while k < r and flag:
+        if col[r] == col[k] or abs(col[r] - col[k]) == (r - k):
             return False
+        k += 1
     return True
 
-def dfs(x):
-    global result
 
-    if x == N:
-        result += 1
-    else:
-        # 각 행에 퀸 놓아보기
-        for i in range(N):
-            row[x] = i
-            # 행, 열 대각선 체크 true 반환시 다음 줄 체크
-            # 백트래킹
-            if adjacent(x):
-                dfs(x+1)
-
-
-
-N = int(input())
-row = [0] * N
-result = 0
-
-dfs(0)
-print(result)
+n_queen(0, col)
+print(cnt)
