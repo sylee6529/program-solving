f9f9c5440985ed491d54f1e6ab60fe9c91d4493f
Seoyeon Lee <68765200+sylee6529@users.noreply.github.com>
2023-10-30 11:19:30 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,15 +1,14 @@
 import sys
 
 n = int(sys.stdin.readline())
-dp = [0, 1] + [0] * (n-1)
+
 
 
 def fibo(n):
-    if n <= 1:
-        return n
-    
-    if dp[n] == 0:
-        dp[n] = fibo(n-1) + fibo(n-2)
+    dp = [0, 1] + [0] * (n - 1)
+
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
     return dp[n]
 
 
